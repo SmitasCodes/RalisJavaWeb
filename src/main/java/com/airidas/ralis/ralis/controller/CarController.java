7cddd125fa159636be6f18907b3eda9a7481f473
SmitasCodes <airidassmith30@gmail.com>
2024-11-12 12:02:05 +0200

    added controller

---
+++ src/main/java/com/airidas/ralis/ralis/controller/CarController.java
@@ -0,0 +1,46 @@
+package com.airidas.ralis.ralis.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.airidas.ralis.ralis.model.Masina;
+import com.airidas.ralis.ralis.service.CarService;
+
+@Controller
+@RequestMapping("/cars")
+public class CarController {
+
+    @Autowired
+    private CarService carService;
+
+    @GetMapping
+    public String showCars(Model model) {
+        model.addAttribute("cars", carService.getAllCars());
+        return "car-list";
+    }
+
+    @GetMapping("/average-volume")
+    public String showAverageVolume(Model model) {
+        model.addAttribute("averageVolume", carService.calculateAverageVolume());
+        return "average-volume";
+    }
+
+    @GetMapping("/search")
+    public String searchForm() {
+        return "search-form";
+    }
+
+    @PostMapping("/search")
+    public String searchByMakeAndModel(@RequestParam String make, @RequestParam String model, Model modelAttr) {
+        List<Masina> results = carService.searchByMakeAndModel(make, model);
+        modelAttr.addAttribute("cars", results);
+        return "car-list";
+    }
+}
